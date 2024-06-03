6a6ab145a3df4f9dee943019ea94b28d1c631fb8
Jay Manek <manekj27@gmail.com>
2024-06-04 02:14:07 +0530

    Day 04 4-6-24

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react'
 import './Intro.css';
 export default function Intro() {
@@ -9,6 +10,9 @@ export default function Intro() {
                 <h2>
                     I want to <span class="highlight">make things</span><br />that <span class="underline">make a difference</span>.
                 </h2>
+                <div class="container-image">
+                  <img src="https://images.bitmoji.com/3d/avatar/451978050-556730612_13-s5-v1.webp?ua=2"/>
+                </div>
             </div>
     </section>
     </>

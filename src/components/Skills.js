09ef3bfe348afbe6b84bf1ef99d54ab2f6ad45c5
Jay Manek <manekj27@gmail.com>
2024-06-11 01:09:15 +0530

    Day 12 11-6-24

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -9,116 +9,68 @@ export default function Skills() {
     <section id="skills" class="section scrollspy">
     <h3 class="page-title white-text teal">Skills</h3>
     <div class="container">
-      <div class="card">
-        <div class="card-content">
-          <h4 class="brown-text light">HTML</h4>
+      
+        <div class="card">
+          <div class="card-content">
+          <h4 class="brown-text light">Languages</h4>
           <div class="row text-center">
             <div class="col s4 m2">
-              <img
-                alt=""
-                src="/assets/html5.png"
-                class="responsive-img"
-              />HTML5
-            </div>
+          <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer"> 
+            <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="60" height="60"/> 
+          </a>
+          <a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer"> 
+            <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="60" height="60"/> 
+          </a>
+          <a href="https://getbootstrap.com" target="_blank" rel="noreferrer"> 
+            <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg" alt="bootstrap" width="60" height="60"/> 
+          </a> 
+          <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"> 
+            <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="60" height="60"/> 
+          </a>
+          <a href="https://reactjs.org/" target="_blank" rel="noreferrer"> 
+            <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="60" height="60"/>
+          </a>
+          <a href="https://www.mysql.com/" target="_blank" rel="noreferrer"> 
+            <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" alt="mysql" width="60" height="60"/> 
+          </a> 
+          <a href="https://www.php.net" target="_blank" rel="noreferrer"> 
+            <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/php/php-original.svg" alt="php" width="60" height="60"/> 
+          </a> 
+          <a href="https://www.python.org" target="_blank" rel="noreferrer"> 
+            <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="60" height="60"/> 
+          </a> 
+          <a href="https://www.w3schools.com/cpp/" target="_blank" rel="noreferrer"> 
+            <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg" alt="cplusplus" width="60" height="60"/>
+          </a>
+      </div>
+      </div>
+      </div>
+      </div>
+      <div class="card">
+        <div class="card-content">
+          <h4 class="brown-text light">Tools</h4>
+          <div class="row text-center">
             <div class="col s4 m2">
-              <img
-                alt=""
-                src="/assets/react.png"
-                class="responsive-img"
-              />JSX
+              <a href="https://www.figma.com/" target="_blank" rel="noreferrer"> 
+                <img src="https://www.vectorlogo.zone/logos/figma/figma-icon.svg" alt="figma" width="60" height="60"/> 
+              </a> 
+              <a href="https://firebase.google.com/" target="_blank" rel="noreferrer"> 
+                <img src="https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg" alt="firebase" width="60" height="60"/> 
+              </a>
+              <a href="https://www.arduino.cc/" target="_blank" rel="noreferrer"> 
+                <img src="https://cdn.worldvectorlogo.com/logos/arduino-1.svg" alt="arduino" width="60" height="60"/> 
+              </a>
+              <a href="https://git-scm.com/" target="_blank" rel="noreferrer"> 
+                <img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" width="60" height="60"/> 
+              </a>
+              <a href="https://ifttt.com/" target="_blank" rel="noreferrer"> 
+                <img src="https://www.vectorlogo.zone/logos/ifttt/ifttt-ar21.svg" alt="ifttt" width="60" height="60"/> 
+              </a>
+              <a href="https://code.visualstudio.com/" target="_blank" rel="noreferrer"> 
+                <img src="https://www.vectorlogo.zone/logos/visualstudio_code/visualstudio_code-icon.svg" alt="vscode" width="60" height="60"/> 
+              </a>
             </div>
           </div>
-        </div>
-      </div>
-      <div class="card">
-        <div class="card-content">
-          <h4 class="brown-text light">CSS</h4>
-          <div class="row text-center">
-            <div class="col s4 m2">
-              <img
-                alt=""
-                src="/assets/css3.png"
-                class="responsive-img"
-              />CSS3
-            </div>
-            <div class="col s4 m2">
-              <img
-                alt=""
-                src="/assets/bootstrap.png"
-                class="responsive-img"
-              />Bootstrap
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="card">
-        <div class="card-content">
-          <h4 class="brown-text light">Javascript</h4>
-          <div class="row text-center">
-            <div class="col s4 m2">
-              <img
-                alt=""
-                src="/assets/react.png"
-                class="responsive-img"
-              />React
-            </div>
-            <div class="col s4 m2">
-              <img
-                alt=""
-                src="/assets/img/javascript.png"
-                class="responsive-img"
-              />Javascript
-            </div>
-            <div class="col s4 m2">
-              <img
-                alt=""
-                src="/assets/jquery.png"
-                class="responsive-img"
-              />jQuery
-            </div>
-          </div>
-        </div>
-      </div>
-      </div>
-      <div class="card">
-        <div class="card-content">
-          <h4 class="brown-text light">Build Tools</h4>
-          <div class="row text-center">
-            <div class="col s4 m2">
-              <img
-                alt=""
-                src="/assets/webpack.png"
-                class="responsive-img"
-              />Visual Studio
-            </div>
-            <div class="col s4 m2">
-              <img
-                alt=""
-                src="/assets/gulp.png"
-                class="responsive-img"
-              />Figma
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="card">
-        <div class="card-content">
-          <h4 class="brown-text light">Other</h4>
-          <div class="row text-center">
-            <div class="col s4 m2">
-              <img
-                alt=""
-                src="/assets/github.png"
-                class="responsive-img"
-              />Github
-            </div>
-            <div class="col s4 m2">
-              <img
-                alt=""
-                src="/assets/firebase.png"
-                class="responsive-img"
-              />Firebase
-            </div>
           </div>
         </div>
       </div>
